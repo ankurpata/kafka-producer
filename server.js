7514f35eb1553c216a11761e25d50fc91fa6d9ca
Ankur Patankar <ankur.pata@gmail.com>
2020-06-01 22:12:15 +0400

    add iteration no

--- server.js
+++ server.js
@@ -57,20 +57,22 @@ app.get('/price_revision', async (req, res) => {
     // Convert CSV string into rows:
     const rows = await csv.parse(csvString);
     let i = 1;
+    const intNo = (new Date()).getTime().toString();
+
     /**
      * Log time before dispatching
      */
     const logParams = {
         "iterationName": "price_revision_insert",
-        "iterationNumber": (new Date()).getTime().toString(),
+        "iterationNumber": intNo,
         "numRecords": rows.length,
-        "execTime1":-1,
+        "execTime1": -1,
         "startTime": new Date().toISOString(),
         "endTime": new Date().toISOString(),
         "batchNumber": 1,
         "itemsPerBatch": rows.length,
         "execTime2": "-1",
-        "platform":"reaction"
+        "platform": "reaction"
     };
     try {
 
@@ -85,8 +87,10 @@ app.get('/price_revision', async (req, res) => {
     const TOPIC = "PRICE_REVISION";
     const partition = -1;
     const key = 'KEY-PRICE';
-    let value = Buffer.from(JSON.stringify(rows));
-    producer.produce(TOPIC, partition, value, key);
+    let payload = {data: rows, intNo};
+    payload = Buffer.from(JSON.stringify(payload));
+
+    producer.produce(TOPIC, partition, payload, key);
 
     console.log('Dipatched CSV data', rows.length);
     await wait(5000);
@@ -97,23 +101,24 @@ app.get('/price_revision', async (req, res) => {
 /**
  * CSV import producer
  */
-app.get('/csv', async(req, res) => {
+app.get('/csv', async (req, res) => {
 
-    const csvSize= req.query.size;
+    const csvSize = req.query.size;
     /**
      * Log time before dispatching
      */
+    const intNo = (new Date()).getTime().toString();
     const logParams = {
         "iterationName": "csv_produts_insert",
-        "iterationNumber": (new Date()).getTime().toString(),
+        "iterationNumber": intNo,
         "numRecords": 0,
-        "execTime1":-1,
+        "execTime1": -1,
         "startTime": new Date().toISOString(),
         "endTime": new Date().toISOString(),
         "batchNumber": 1,
         "itemsPerBatch": 0,
         "execTime2": "-1",
-        "platform":"reaction"
+        "platform": "reaction"
     };
     try {
 
@@ -127,10 +132,11 @@ app.get('/csv', async(req, res) => {
     const TOPIC = "IMPORT_CSV";
     const partition = -1;
     const key = 'KEY-1';
-    let value = csvSize == 'big'? "UPLOAD_CSV_BIG" : "UPLOAD_CSV";
-    value = Buffer.from(JSON.stringify(value));
+    let value = csvSize == 'big' ? "UPLOAD_CSV_BIG" : "UPLOAD_CSV";
+    let payload = {value, intNo};
+    payload = Buffer.from(JSON.stringify(payload));
 
-    producer.produce(TOPIC, partition, value, key);
+    producer.produce(TOPIC, partition, payload, key);
     res.json({res: "Dispatched CSV data"});
     return;
 
